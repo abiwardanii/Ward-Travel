1171c2134dadc554500811e224c8cc4eb5d1f468
unknown <yohangala44@gmail.com>
2021-02-13 10:13:27 +0700

    init repo

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,47 @@
+<footer class="section-footer mt-5 mb-4 border-top">
+        <div class="container pt-5 pb-5">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="row">
+                        <!-- 1 -->
+                        <div class="col-12 col-lg-3">
+                            <h5>FEATURES</h5>
+                            <ul class="list-unstyled">
+                                <a href="#"><li>Reviews</li></a>
+                                <a href="#"><li>Company</li></a>
+                                <a href="#"><li>Sosial Media Kit</li></a>
+                                <a href="#"><li>Affiliate</li></a>
+                            </ul>
+                        </div>
+                        <!-- 2 --><div class="col-12 col-lg-3">
+                            <h5>ACCOUNT</h5>
+                            <ul class="list-unstyled">
+                                <a href="#"><li>Refund</li></a>
+                                <a href="#"><li>Security</li></a>
+                                <a href="#"><li>Reward</li></a>
+                            </ul>
+                        </div>
+                        <!-- 3 -->
+                        <div class="col-12 col-lg-3">
+                            <h5>COMPANY</h5>
+                            <ul class="list-unstyled">
+                                <a href="#"><li>Carrer</li></a>
+                                <a href="#"><li>Help Center</li></a>
+                                <a href="#"><li>Media</li></a>
+                            </ul>
+                        </div>
+                        <!-- 4 -->
+                        <div class="col-12 col-lg-3">
+                            <h5>GET CONNECTED</h5>
+                            <ul class="list-unstyled">
+                                <a href="#"><li>Malang</li></a>
+                                <a href="#"><li>Indonesia</li></a>
+                                <a href="#"><li>0822-4444-6666</li></a>
+                                <a href="#"><li>wardtravel@gmail.com</li></a> 
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
